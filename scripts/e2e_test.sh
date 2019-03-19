42d5e846a05b8513d1e20a0bc8d85dcd160c61e0
yxfeng <yxfeng@thoughtworks.com>
2019-03-19 22:49:07 +0800

    fix script

--- scripts/e2e_test.sh
+++ scripts/e2e_test.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
-curl -O -L https://npmjs.org/install.sh
-sh install.sh
-sudo ln -s /usr/local/bin/node /usr/bin/node
-sudo ln -s /usr/local/lib/node /usr/lib/node
-sudo ln -s /usr/local/bin/npm /usr/bin/npm
-sudo ln -s /usr/local/bin/node-waf /usr/bin/node-waf
+apt-get install npm
 echo "e2e test start."
 npm run e2e
 echo "e2e test end."
