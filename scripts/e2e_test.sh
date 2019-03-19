9278d50a25a01e03f16ab82e0e01a0b98b9a8ecc
yxfeng <yxfeng@thoughtworks.com>
2019-03-19 21:27:39 +0800

    install npm in script

--- scripts/e2e_test.sh
+++ scripts/e2e_test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+curl http://npmjs.org/install.sh | sh
 echo "e2e test start."
 npm run e2e
 echo "e2e test end."
--- scripts/unit_test.sh
+++ scripts/unit_test.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+curl http://npmjs.org/install.sh | sh
 echo "unit test start."
 npm run test
 echo "unit test end."
