edf16a0253524d01534e5cebf28848ae0673fc2a
yxfeng <yxfeng@thoughtworks.com>
2019-03-19 22:58:10 +0800

    fix script

--- scripts/e2e_test.sh
+++ scripts/e2e_test.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-apt-get install npm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 11.3.0
 echo "e2e test start."
 npm run e2e
 echo "e2e test end."
