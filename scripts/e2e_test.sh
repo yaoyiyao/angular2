fba28c8ffff8f84864276e8372ff53ccc2573be9
yxfeng <yxfeng@thoughtworks.com>
2019-03-19 22:30:42 +0800

    fix script

--- scripts/e2e_test.sh
+++ scripts/e2e_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-curl http://npmjs.org/install.sh | sh
-curl https://npmjs.org/install.sh | sh
+curl -O -L https://npmjs.org/install.sh
+sudo sh install.sh
 echo "e2e test start."
 npm run e2e
 echo "e2e test end."
