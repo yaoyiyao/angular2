039a63a795ce3d9244359819a095e3bc75c20708
yxfeng <yxfeng@thoughtworks.com>
2019-03-19 23:05:59 +0800

    fix script

--- scripts/unit_test.sh
+++ scripts/unit_test.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-curl http://npmjs.org/install.sh | sh
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 11.3.0
 echo "unit test start."
+npm install
 npm run test
 echo "unit test end."
