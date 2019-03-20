7edcd2c70f47c46de1dae7c04c98835b1f24cb5f
yxfeng <yxfeng@thoughtworks.com>
2019-03-20 19:45:56 +0800

    change script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "build start."
-export PATH = "/home/ec2-user/.nvm/versions/node/v11.12.0/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:/home/ec2-user/.local/bin:/home/ec2-user/bin"
+export PATH="/home/ec2-user/.nvm/versions/node/v11.12.0/bin:/usr/local/bin:/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/sbin:/opt/aws/bin:/home/ec2-user/.local/bin:/home/ec2-user/bin"
 npm install
 npm run build
 echo "build end."
