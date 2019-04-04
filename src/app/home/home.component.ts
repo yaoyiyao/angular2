9fbb8890e7531fd39d0f8df1f4442f433de28cb6
yxfeng <yxfeng@thoughtworks.com>
2019-04-04 16:06:08 +0800

    [Yixuan] spike table library

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  text = 'home works';
+  text = 'home works cool';
 
   constructor() { }
 
